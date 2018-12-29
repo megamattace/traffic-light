9298b91c1bf38cd77cb8811d6d88a6f57d42b75e
Williamrobert Green <(none)>
2018-12-29 10:39:35 -0500

    sets timeout for stop function

--- main.js
+++ main.js
@@ -1,26 +1,22 @@
 // Create reference to buttons
 // https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
 let stopButton = document.querySelector('#stopButton');
-let slowButton = document.querySelector('#slowButton');
 let goButton = document.querySelector('#goButton');
 
 // Create reference to lights
 let red = document.querySelector('#red');
-let yellow = document.querySelector('#yellow');
 let green = document.querySelector('#green');
 
 // Define event handlers
 // https://developer.mozilla.org/en/docs/Web/API/Element/classList
 function stop() {
-  yellow.classList.remove('lit');
   green.classList.remove('lit');
-  red.classList.add('lit');
-}
-
-function slow() {
   red.classList.remove('lit');
-  green.classList.remove('lit');
   yellow.classList.add('lit');
+  setTimeout(function () {
+    yellow.classList.remove('lit');
+    red.classList.add('lit');
+  }, 1000)
 }
 
 function go() {
@@ -32,5 +28,4 @@ function go() {
 // Create event listeners
 // https://developer.mozilla.org/en-US/docs/Talk:DOM/element.addEventListener
 stopButton.addEventListener('click', stop);
-slowButton.addEventListener('click', slow);
 goButton.addEventListener('click', go);
