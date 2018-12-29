c8b4811ba4e54a26477b485d101315f9d3a3df60
Williamrobert Green <(none)>
2018-12-29 10:40:39 -0500

    removes references from js

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 // Create reference to buttons
-// https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
 
 // Define event handlers
-// https://developer.mozilla.org/en/docs/Web/API/Element/classList
 
 // Create event listeners
-// https://developer.mozilla.org/en-US/docs/Talk:DOM/element.addEventListener
